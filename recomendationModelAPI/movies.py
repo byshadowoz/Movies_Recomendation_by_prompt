2285ec09eabf89e9d52aaa54e4ac40079a837d26
BYSHDWZ <cesardleonesm@gmail.com>
2024-06-11 12:19:52 -0400

    creating recomendation model with API

---
+++ recomendationModelAPI/movies.py
@@ -0,0 +1,45 @@
+import requests
+import os
+
+url = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1"
+api_key = os.environ['TMDB_API_KEY']
+
+headers = {
+    "accept": "application/json",
+    "Authorization": "Bearer {api_key}"
+}
+
+response = requests.get(url, headers=headers)
+
+def movies_id(res):# return all the movies in the page in a dictionary where key = id and value = title
+    try:
+        movies_dict = {}
+        data = res.json()
+        movies = data['results']
+        for movie in movies:
+            movies_dict[movie['id']] = movie['title']
+        return movies_dict
+    
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        print(f'response status code: {response.status_code}')
+
+def change_page(response, url):
+    newUrl = url.split('page=')
+        
+    if len(newUrl) <= 1:
+        return 'invalid URL'
+        
+    page = str(input('select num page '))
+    newUrl[-1] = page
+    newUrl = "page=".join(newUrl)
+        
+    response = requests.get(newUrl, headers=headers)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print('error, page does not exist')
+        
+    
+
+change_page(response,url)
