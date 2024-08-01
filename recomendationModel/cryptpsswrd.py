7c8691bc4a5e3651003900af98f747ac41394e78
BYSHDWZ <cesardleonesm@gmail.com>
2024-08-01 11:47:09 -0400

    ambiguos change

--- recomendationModel/cryptpsswrd.py
+++
@@ -1,9 +0,0 @@
-import bcrypt
-def encryptPassword(password):
-    pwd = password.encode('utf-8')
-    sal = bcrypt.gensalt()
-    encrypt = bcrypt.hashpw(pwd, sal)
-    return encrypt
-
-def uncryptPassword(Password, hash):
-    return bcrypt.checkpw(Password.encode('utf-8'), hash)
